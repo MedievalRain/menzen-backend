151e9ed5982bb14d43f8e40f11ca6005ead3738e
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 10:17:37 +0300

    Create new user in database

---
+++ src/domain/user/userRepository.ts
@@ -0,0 +1,21 @@
+import { sql } from "../../config/database/connection";
+import { UserExistsError } from "../../errors/UserExistsError";
+
+export class UserRepository {
+  public async createUser(email: string, passwordHash: string) {
+    try {
+      const [row] = await sql`INSERT INTO users ${sql({
+        email,
+        passwordHash,
+        isVerified: false,
+      })} RETURNING id`;
+      return row.id as string;
+    } catch (error) {
+      if (error instanceof sql.PostgresError) {
+        if (error.code === "23505") {
+          throw new UserExistsError();
+        }
+      }
+    }
+  }
+}
