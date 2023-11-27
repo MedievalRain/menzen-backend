e513be65e8022f93687eda81aa7dac5ac6684717
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 11:49:09 +0300

    Fix status code

--- src/errors/UserNotExistsError.ts
+++ src/errors/UserNotExistsError.ts
@@ -2,7 +2,7 @@ import { AppError } from "./appError";
 
 export class UserNotExistsError extends AppError {
   constructor(message: string = "User already exists") {
-    super(message, 409, "USER_NOT_EXISTS");
+    super(message, 404, "USER_NOT_EXISTS");
     Object.setPrototypeOf(this, new.target.prototype);
   }
 }
