25bf56d6508e9b77805a4bd8bdb9fce480b7ebea
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 11:22:23 +0300

    Verify user email route

--- src/api/routes/userRouter.ts
+++ src/api/routes/userRouter.ts
@@ -11,3 +11,12 @@ userRouter.post("/register", async (req, res, next) => {
     next(error);
   }
 });
+
+userRouter.get("/verify", async (req, res, next) => {
+  try {
+    await userService.verifyEmail(req.params);
+    res.status(200).json({ message: "user verified" });
+  } catch (error) {
+    next(error);
+  }
+});
