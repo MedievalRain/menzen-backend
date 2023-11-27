6577a4f60bf23cb5426ac7108ea12e74e0437e20
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 11:12:50 +0300

    Add error when user is already verified

--- src/errors/errorTypes.ts
+++ src/errors/errorTypes.ts
@@ -1,1 +1,8 @@
-export type ErrorTypes = "JWT" | "WRONG_PASSWORD" | "VALIDATION_ERROR" | "USER_EXISTS" | "USER_NOT_EXISTS" | "USER_NOT_ACTIVE";
+export type ErrorTypes =
+  | "JWT"
+  | "WRONG_PASSWORD"
+  | "VALIDATION_ERROR"
+  | "USER_EXISTS"
+  | "USER_NOT_EXISTS"
+  | "USER_NOT_ACTIVE"
+  | "USER_VERIFIED";
