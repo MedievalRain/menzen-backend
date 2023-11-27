e25c324769bb765339d55f4668c220313ef9633c
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 10:22:50 +0300

    Rename function

--- src/domain/user/authUtils.ts
+++ src/domain/user/authUtils.ts
@@ -1,6 +1,6 @@
 import * as bcrypt from "bcrypt";
 
-export const generateHash = async (password: string) => {
+export const generatePasswordHash = async (password: string) => {
   return await bcrypt.hash(password, 10);
 };
 export const validatePassword = async (inputPassword: string, hashedPassword: string): Promise<boolean> => {
