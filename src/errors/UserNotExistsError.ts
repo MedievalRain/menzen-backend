dbec602196588d789ca5f8d2af7f703bbcd05c5d
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 16:18:08 +0300

    Fix message

--- src/errors/UserNotExistsError.ts
+++ src/errors/UserNotExistsError.ts
@@ -1,7 +1,7 @@
 import { AppError } from "./appError";
 
 export class UserNotExistsError extends AppError {
-  constructor(message: string = "User already exists") {
+  constructor(message: string = "User does not exist") {
     super(message, 404, "USER_NOT_EXISTS");
     Object.setPrototypeOf(this, new.target.prototype);
   }
