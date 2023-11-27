7c3ecb2e24a3eb213079f13332ae3a7642aac992
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 14:38:54 +0300

    Fix middleware order

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,12 +3,15 @@ import express from "express";
 import { corsOptions } from "../config/cors";
 import cors from "cors";
 import { errorMiddleware } from "../middleware/error";
+import { userRouter } from "./routes/userRouter";
 
 const apiRouter = express.Router();
 apiRouter.use(cookieParser());
 apiRouter.use(cors(corsOptions));
 apiRouter.use(express.json());
 apiRouter.use(express.urlencoded({ extended: true }));
+
+apiRouter.use("/user", userRouter);
 
 apiRouter.use(errorMiddleware);
 
