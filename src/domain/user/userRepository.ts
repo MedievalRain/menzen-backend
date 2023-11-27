a0f0597be130f42b16b97db4454577388cd06c59
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 11:06:22 +0300

    Type database response

--- src/domain/user/userRepository.ts
+++ src/domain/user/userRepository.ts
@@ -1,15 +1,16 @@
 import { sql } from "../../config/database/connection";
 import { UserExistsError } from "../../errors/UserExistsError";
+import { IdResponse } from "./userTypes";
 
 export class UserRepository {
   public async createUser(email: string, passwordHash: string): Promise<string> {
     try {
-      const [row] = await sql`INSERT INTO users ${sql({
+      const rows = await sql<IdResponse[]>`INSERT INTO users ${sql({
         email,
         passwordHash,
         isVerified: false,
       })} RETURNING id`;
-      return row.id as string;
+      return rows[0].id;
     } catch (error) {
       if (error instanceof sql.PostgresError) {
         if (error.code === "23505") {
