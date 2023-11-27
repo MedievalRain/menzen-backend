da1a985fbcfae6f69324505127693c4c4fdf76bd
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 08:18:50 +0300

    Add JWT_KEY env variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const POSTGRES_URL = process.env.POSTGRES_URL!;
+export const JWT_KEY = process.env.JWT_KEY!;
 
-if (!POSTGRES_URL) {
+if (!(POSTGRES_URL && JWT_KEY)) {
   throw new Error("One of environment variables is missing");
 }
