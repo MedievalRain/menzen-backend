ffe83fda587c3ea1b8e6f89b6502085bffb44a3a
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-27 10:35:14 +0300

    Create user router

--- src/api/routes/userRouter.ts
+++ src/api/routes/userRouter.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import { userService } from "../../domain/user/userService";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", async (req, res, next) => {
   try {
+    await userService.createUser(req.body);
     res.status(200).json({ message: "user registered" });
   } catch (error) {
     next(error);
