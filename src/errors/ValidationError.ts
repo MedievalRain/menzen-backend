ee2d4099ebf51a7901945adf864ce9e9b761013d
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-30 10:11:03 +0300

    Fix error code

--- src/errors/ValidationError.ts
+++ src/errors/ValidationError.ts
@@ -2,7 +2,7 @@ import { AppError } from "./appError";
 
 export class ValidationError extends AppError {
   constructor(message: string = "Validation error") {
-    super(message, 409, "VALIDATION_ERROR");
+    super(message, 400, "VALIDATION_ERROR");
     Object.setPrototypeOf(this, new.target.prototype);
   }
 }
