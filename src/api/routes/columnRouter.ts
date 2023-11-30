34687f5aa2ffdf1ab6cf6c1ee45d9b0fa8d1f8c0
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-30 10:13:44 +0300

    Fix request data passing

--- src/api/routes/columnRouter.ts
+++ src/api/routes/columnRouter.ts
@@ -33,7 +33,7 @@ columnRouter.delete("/", async (req, res, next) => {
 
 columnRouter.post("/", async (req, res, next) => {
   try {
-    await columnService.renameColumn(req.query, res.locals.userId);
+    await columnService.renameColumn(req.body, res.locals.userId);
     res.status(200).json({ message: "Column renamed" });
   } catch (error) {
     next(error);
