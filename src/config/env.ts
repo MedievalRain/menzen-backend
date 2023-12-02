7617a3366d443140b650e78ef549752dbe37cd71
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-02 17:02:36 +0300

    Load bucket URL env variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -8,6 +8,7 @@ export const PORT = process.env.PORT!;
 export const RESEND_KEY = process.env.RESEND_KEY!;
 export const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY!;
 export const S3_SECRET_KEY = process.env.S3_SECRET_KEY!;
-if (!(POSTGRES_URL && JWT_KEY && FRONTEND_DOMAIN && PORT && RESEND_KEY && S3_ACCESS_KEY && S3_SECRET_KEY)) {
+export const BUCKET_API_URL = process.env.BUCKET_API_URL!;
+if (!(POSTGRES_URL && JWT_KEY && FRONTEND_DOMAIN && PORT && RESEND_KEY && S3_ACCESS_KEY && S3_SECRET_KEY && BUCKET_API_URL)) {
   throw new Error("One of environment variables is missing");
 }
