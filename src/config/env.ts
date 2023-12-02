5014129bf480f8fb39e2ca1f681894d0e493d6dc
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-02 16:59:46 +0300

    Load S3 API keys

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,7 +6,8 @@ export const JWT_KEY = process.env.JWT_KEY!;
 export const FRONTEND_DOMAIN = process.env.FRONTEND_DOMAIN!;
 export const PORT = process.env.PORT!;
 export const RESEND_KEY = process.env.RESEND_KEY!;
-
-if (!(POSTGRES_URL && JWT_KEY && FRONTEND_DOMAIN && PORT && RESEND_KEY)) {
+export const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY!;
+export const S3_SECRET_KEY = process.env.S3_SECRET_KEY!;
+if (!(POSTGRES_URL && JWT_KEY && FRONTEND_DOMAIN && PORT && RESEND_KEY && S3_ACCESS_KEY && S3_SECRET_KEY)) {
   throw new Error("One of environment variables is missing");
 }
