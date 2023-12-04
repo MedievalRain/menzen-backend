4624da4d70e89cd1e96ea24080621e929994d76b
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-04 05:02:16 +0300

    update endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { apiRouter } from "./api/api";
 
 const app = express();
-app.use("/api", apiRouter);
+app.use("/menzen", apiRouter);
 app.listen(8000, () => {
   console.log(`⚡️[server]: Server is running at http://127.0.0.1:${8000}`);
 });
