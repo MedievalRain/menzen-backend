8f52a4cf3a69a00bbdaa71b277fb6f5ace132e12
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-04 03:25:50 +0300

    Remove PORT env

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,6 @@ dotenv.config();
 export const POSTGRES_URL = process.env.POSTGRES_URL!;
 export const JWT_KEY = process.env.JWT_KEY!;
 export const FRONTEND_DOMAIN = process.env.FRONTEND_DOMAIN!;
-export const PORT = process.env.PORT!;
 export const RESEND_KEY = process.env.RESEND_KEY!;
 export const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY!;
 export const S3_SECRET_KEY = process.env.S3_SECRET_KEY!;
@@ -12,17 +11,7 @@ export const BUCKET_API_URL = process.env.BUCKET_API_URL!;
 export const BUCKET_NAME = process.env.BUCKET_NAME!;
 
 if (
-  !(
-    POSTGRES_URL &&
-    JWT_KEY &&
-    FRONTEND_DOMAIN &&
-    PORT &&
-    RESEND_KEY &&
-    S3_ACCESS_KEY &&
-    S3_SECRET_KEY &&
-    BUCKET_API_URL &&
-    BUCKET_NAME
-  )
+  !(POSTGRES_URL && JWT_KEY && FRONTEND_DOMAIN && RESEND_KEY && S3_ACCESS_KEY && S3_SECRET_KEY && BUCKET_API_URL && BUCKET_NAME)
 ) {
   throw new Error("One of environment variables is missing");
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import express from "express";
 import { apiRouter } from "./api/api";
-import { PORT } from "./config/env";
 
 const app = express();
 app.use("/api", apiRouter);
-app.listen(PORT, () => {
+app.listen(8000, () => {
   console.log(`⚡️[server]: Server is running at http://127.0.0.1:${PORT}`);
 });
